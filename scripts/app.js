8c5ca8fc38f4b66c779507525267b44b784105dc
prince <princechorasiya6@gmail.com>
2024-07-05 13:40:22 +0530

    contact/product done

--- scripts/app.js
+++ scripts/app.js
@@ -73,3 +73,27 @@ setInterval(() => {
 }, 8000); // Change slide every 5 seconds
 
 window.onload = startTyping;
+
+//faq's
+document.querySelectorAll(".faq-question").forEach((question) => {
+	question.addEventListener("click", () => {
+		// Close any other open answers
+		document
+			.querySelectorAll(".faq-question.active")
+			.forEach((activeQuestion) => {
+				if (activeQuestion !== question) {
+					activeQuestion.classList.remove("active");
+					activeQuestion.nextElementSibling.style.display = "none";
+				}
+			});
+
+		// Toggle the clicked question
+		question.classList.toggle("active");
+		const answer = question.nextElementSibling;
+		if (question.classList.contains("active")) {
+			answer.style.display = "block";
+		} else {
+			answer.style.display = "none";
+		}
+	});
+});
