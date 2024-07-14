8fd69c4e79c872c606f730ef6fbe329022f10c85
prince <princechorasiya6@gmail.com>
2024-07-14 15:22:54 +0530

    product-fixes

--- pages/product/products/script.js
+++ pages/product/products/script.js
@@ -12,40 +12,6 @@ var swiper = new Swiper(".blog-slider", {
 	},
 });
 
-const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
-
-function smoothMoves(event) {
-	const mouseX = event.clientX;
-	const mouseY = event.clientY;
-
-	gsap.to(circle, { duration: 0.5, attr: { cx: mouseX, cy: mouseY } });
-}
-
-function expandHero() {
-	tl.to(circle, {
-		duration: 1,
-		attr: {
-			r: "2000",
-		},
-		ease: "power2.inOut",
-	})
-		.to(
-			hero,
-			{
-				duration: 1,
-				scale: 1,
-				borderRadius: 0,
-				ease: "power2.inOut",
-			},
-			"-=1"
-		)
-		.to(body, {
-			overflow: "auto",
-		});
-}
-
-document.addEventListener("mousemove", smoothMoves);
-
 // hero.addEventListener("click", expandHero);
 
 // add the slider for gallery
